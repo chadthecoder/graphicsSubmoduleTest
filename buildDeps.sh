87d3a2ff2d6e62d5fa24c722b7403354a9559166
chadthecoder <mchadamschad@outlook.com>
2019-06-27 21:20:43 -0700

    added building glfw to linux sh file

--- buildDeps.sh
+++ buildDeps.sh
@@ -5,10 +5,7 @@ cmake ..
 make
 cd ../../..
 
-cd deps/glew
-gcc -DGLEW_NO_GLU -O2 -Wall -W -Iinclude  -DGLEW_BUILD -o src/glew.o -c src/glew.c
-gcc -shared -Wl,-soname,libglew32.dll -Wl,--out-implib,lib/libglew32.dll.a    -o lib/glew32.dll src/glew.o -L/mingw/lib -lglu32 -lopengl32 -lgdi32 -luser32 -lkernel32
-ar cr lib/libglew32.a src/glew.o
-cd ../../..
-
-
+cd deps/glfw
+cmake .
+make
+cd ../..
